3103eb20a346e3b6514fc398637c1bd1a5bbcdf1
wilgao09 <william.gao09@gmail.com>
2022-12-30 18:00:59 -0500

    modified build script

--- scripts/brepo.js
+++ scripts/brepo.js
@@ -2,6 +2,7 @@ import f_00000000t from "./00000000t.md"
 import f_20220611The_First_Actual_Blog_Post from "./20220611The First Actual Blog Post.md"
 import f_20220802Midnight_Ideas_and_a_Better_Blog from "./20220802Midnight Ideas and a Better Blog.md"
 import f_20221224Project_Direction from "./20221224Project Direction.md"
+import f_20221230Tracker_and_Java_Spring_Boot from "./20221230Tracker and Java Spring Boot.md"
 
 
 const Blogs={
@@ -9,6 +10,7 @@ const Blogs={
 "20220611The First Actual Blog Post":f_20220611The_First_Actual_Blog_Post,
 "20220802Midnight Ideas and a Better Blog":f_20220802Midnight_Ideas_and_a_Better_Blog,
 "20221224Project Direction":f_20221224Project_Direction,
+"20221230Tracker and Java Spring Boot":f_20221230Tracker_and_Java_Spring_Boot,
 }
 
 export default Blogs
