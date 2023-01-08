2947fcf5b4384b99eabfd69e3b9e9bc8d35dc426
wilgao09 <william.gao09@gmail.com>
2023-01-07 23:23:46 -0500

    updated projects page

--- pages/projects.js
+++ pages/projects.js
@@ -5,51 +5,73 @@ import Layout from "../components/Clayout";
 import MContent from "../components/MContent";
 
 export default function Projects(props) {
-  return (
-    <Layout terminal={props.terminal} location={"/projects"} title="Projects">
-      <Head>
-        <title> Projects </title>
-      </Head>
-      <Header tno={3} terminal={props.terminal} />
+    return (
+        <Layout
+            terminal={props.terminal}
+            location={"/projects"}
+            title="Projects"
+        >
+            <Head>
+                <title> Projects </title>
+            </Head>
+            <Header tno={3} terminal={props.terminal} />
 
-      <MContent>
-        <p>
-          This page is intended to host and showcase the projects that I think
-          are both noteworthy and easily displayable on the web. For a
-          comprehensive list of my projects, please visit{" "}
-          <a
-            href="https://github.com/wilgao09?tab=repositories"
-            target="_blank"
-          >
-            my github
-          </a>
-          .
-        </p>
+            <MContent>
+                <p>
+                    This page is intended to host and showcase the projects that
+                    I think are both noteworthy and easily displayable on the
+                    web. For a comprehensive list of my projects, please visit{" "}
+                    <a href="https://github.com/wilgao09" target="_blank">
+                        my github
+                    </a>
+                    .
+                </p>
 
-        <h3> BetterChatRoom </h3>
-        <p>
-          Here's a link to my chatroom project (I cannot guarantee the security
-          of anything):{" "}
-          <a
-            href="https://chatroom8982.herokuapp.com"
-            style={{ textDecoration: "line-through" }}
-          >
-            https://chatroom8982.herokuapp.com
-          </a>
-        </p>
+                <h3> BetterChatRoom </h3>
+                <p>
+                    Here's a link to my chatroom project (I cannot guarantee the
+                    security of anything):{" "}
+                    <a
+                        href="https://chatroom8982.herokuapp.com"
+                        target="_blank"
+                        style={{ textDecoration: "line-through" }}
+                    >
+                        https://chatroom8982.herokuapp.com
+                    </a>
+                </p>
 
-        <p>
-          Heroku has ended its free service; this project won't be back up until
-          I pay for a proper MongoDB+Digital Ocean instance (at that point, I
-          might as well buy an OK domain name)
-        </p>
+                <p>
+                    Heroku has ended its free service; this project won't be
+                    back up until I pay for a proper MongoDB+Digital Ocean
+                    instance (at that point, I might as well buy an OK domain
+                    name)
+                </p>
 
-        <h3> More </h3>
-        <p>
-          When I have projects that can be compiled to WebAssembly, they'll be
-          showcased here or contain a link to them here.
-        </p>
-      </MContent>
-    </Layout>
-  );
+                <h3>Tracker</h3>
+                <p>
+                    Here's a link to my tracker project. The tracker project's
+                    objective is to aggregate data from users (by observing web
+                    traffic on websites) and to display it nicely. As of writing
+                    this, the Tracker project only tracks the traffic on my
+                    personal website. The project's backend is done in Java
+                    Spring Boot and MySQL; the frontend is done in Typescript,
+                    with the graphs done in VictoryJS. A link to the project can
+                    be found{" "}
+                    <a
+                        href="https://tracker-dashboard.vercel.app"
+                        target="_blank"
+                    >
+                        here
+                    </a>
+                    .
+                </p>
+
+                <h3> More </h3>
+                <p>
+                    When I have projects that can be compiled to WebAssembly,
+                    they'll be showcased here or contain a link to them here.
+                </p>
+            </MContent>
+        </Layout>
+    );
 }
--- scripts/brepo.js
+++ scripts/brepo.js
@@ -7,12 +7,12 @@ import f_20230105The_High_Weirdness_of_Java_Spring_Boot from "./20230105The High
 
 
 const Blogs={
-"00000000t":f_00000000t,
-"20220611The First Actual Blog Post":f_20220611The_First_Actual_Blog_Post,
-"20220802Midnight Ideas and a Better Blog":f_20220802Midnight_Ideas_and_a_Better_Blog,
-"20221224Project Direction":f_20221224Project_Direction,
-"20221230Tracker and Java Spring Boot":f_20221230Tracker_and_Java_Spring_Boot,
-"20230105The High Weirdness of Java Spring Boot":f_20230105The_High_Weirdness_of_Java_Spring_Boot,
+    "00000000t": f_00000000t,
+    "20220611The First Actual Blog Post": f_20220611The_First_Actual_Blog_Post,
+    "20220802Midnight Ideas and a Better Blog": f_20220802Midnight_Ideas_and_a_Better_Blog,
+    "20221224Project Direction": f_20221224Project_Direction,
+    "20221230Tracker and Java Spring Boot": f_20221230Tracker_and_Java_Spring_Boot,
+    "20230105The High Weirdness of Java Spring Boot": f_20230105The_High_Weirdness_of_Java_Spring_Boot,
 }
 
 export default Blogs
