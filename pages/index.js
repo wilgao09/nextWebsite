af2d789f2b49ba2850a0d0f4ec20ff7ab2e796be
wilgao09 <william.gao09@gmail.com>
2023-08-08 11:20:51 -0400

    Updated projects page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,10 @@ export default function Home(props) {
             );
             let commit = await res.json();
             commit = commit[0].commit.author.date;
-            commit = `The last commit to master was on ${commit.slice(0, 10)}`;
+            commit = `The last commit to master was on ${commit.slice(
+                0,
+                10
+            )}`;
 
             githubDateEl.current.innerHTML = commit;
 
@@ -58,7 +61,8 @@ export default function Home(props) {
                         <p>
                             Stony Brook University
                             <br />
-                            Computer Science and Applied Mathematics Major
+                            Computer Science and Applied
+                            Mathematics Major
                         </p>
                         <p></p>
                     </div>
@@ -67,16 +71,55 @@ export default function Home(props) {
                     <div>
                         <h1>Hi</h1>
                         <p>
-                            {" "}
-                            My name is William Gao, and I am a junior currently
-                            studying at Stony Brook University. My current
-                            interests are in compiler design, general software
-                            engineering, algorithmic analysis, and
-                            cybersecurity. Most recently, I have been working
-                            with a friend in developing a compiler for our
-                            Java/C inspired language. Going forward, I wish to
-                            work on larger projects in industry and learn from
-                            more experienced developers.
+                            As a passionate software
+                            engineer with a strong drive for
+                            building software for unique
+                            environments, I am dedicated to
+                            delivering reliable tech
+                            solutions. With a solid
+                            foundation in web technologies
+                            and security, I am committed to
+                            solving novel problems and
+                            thrive in collaborative
+                            environments where innovation
+                            and growth are valued.
+                        </p>
+                        {/* <ul>
+                            <li>
+                                Skills: [Key skills or
+                                expertise that define your
+                                strengths]
+                            </li>
+                            <li>
+                                Passion: [What motivates you
+                                or your main areas of
+                                interest]
+                            </li>
+
+                            <li>
+                                Achievements: [Notable
+                                accomplishments that
+                                showcase your capabilities]
+                            </li>
+                            <li>
+                                Goal: [Your career goal or
+                                mission statement]
+                            </li>
+                        </ul> */}
+
+                        <p
+                            style={{
+                                fontWeight: "bolder",
+                                fontSize: "2ch",
+                            }}
+                        >
+                            I am excited to leverage my
+                            skills and enthusiasm to
+                            construct efficient tech
+                            solutions. Let's connect and
+                            explore the possibilities of how
+                            I can contribute to your name
+                            game-changing project.
                         </p>
                     </div>
 
@@ -84,43 +127,71 @@ export default function Home(props) {
                         <h3> FAQ </h3>
                         <h4> What is this? </h4>
                         <p>
-                            This website is intended to serve multiple purposes.
-                            This website started out as an excuse to learn
-                            React, which very quickly became an excuse to try
-                            out Nextjs and deploying to Vercel. I didn't have
-                            much of an objective at the time, so I was thinking
-                            about making a mockup of what I would have liked for
-                            my personal website to look like. However, I ended
-                            up investing enough time into this that I've decided
-                            that this mock up will serve as my actual personal
-                            website. Since I dont think there's much substance
-                            to put on a personal website, I've also made the
-                            executive decision to make content. This isn't by
-                            any means particularly cool or good content, but I
-                            will sporadically post ramblings on the "Blog" tab.
-                            Hopefully this will make the website feel a little
-                            less empty.
+                            This website is intended to
+                            serve multiple purposes. This
+                            website started out as an excuse
+                            to learn React, which very
+                            quickly became an excuse to try
+                            out Nextjs and deploying to
+                            Vercel. I didn't have much of an
+                            objective at the time, so I was
+                            thinking about making a mockup
+                            of what I would have liked for
+                            my personal website to look
+                            like. However, I ended up
+                            investing enough time into this
+                            that I've decided that this mock
+                            up will serve as my actual
+                            personal website. Since I dont
+                            think there's much substance to
+                            put on a personal website, I've
+                            also made the executive decision
+                            to make content. This isn't by
+                            any means particularly cool or
+                            good content, but I will
+                            sporadically post ramblings on
+                            the "Blog" tab. Hopefully this
+                            will make the website feel a
+                            little less empty.
                         </p>
-                        <h4> Why does the website look like this? </h4>
+                        <h4>
+                            {" "}
+                            Why does the website look like
+                            this?{" "}
+                        </h4>
                         <p>
-                            Honestly, I'm not very good with design, and I
-                            harbor strong feelings against the modern Javascript
-                            ecosystem. It feels incredibly uncomfortable
-                            importing many large javascript front end libraries
-                            just to make clicking a button have a nice effect. I
-                            agree that it's cool, but I don't think its worth
-                            the extra megabyte or two. The color scheme and the
-                            layout are a result of me tinkering around for an
-                            afternoon until I thought it looked "presentable". I
-                            asked for feedback and got incredibly mediocre
-                            reviews, even after adjusting to their criticism.
-                            Getting better at design is a great work in
-                            progress, and I do not expect to become a design god
-                            by finishing this project.
+                            Honestly, I'm not very good with
+                            design, and I harbor strong
+                            feelings against the modern
+                            Javascript ecosystem. It feels
+                            incredibly uncomfortable
+                            importing many large javascript
+                            front end libraries just to make
+                            clicking a button have a nice
+                            effect. I agree that it's cool,
+                            but I don't think its worth the
+                            extra megabyte or two. The color
+                            scheme and the layout are a
+                            result of me tinkering around
+                            for an afternoon until I thought
+                            it looked "presentable". I asked
+                            for feedback and got incredibly
+                            mediocre reviews, even after
+                            adjusting to their criticism.
+                            Getting better at design is a
+                            great work in progress, and I do
+                            not expect to become a design
+                            god by finishing this project.
                         </p>
 
-                        <h4> When was this last updated? </h4>
-                        <p ref={githubDateEl}> Retrieving . . . </p>
+                        <h4>
+                            {" "}
+                            When was this last updated?{" "}
+                        </h4>
+                        <p ref={githubDateEl}>
+                            {" "}
+                            Retrieving . . .{" "}
+                        </p>
                     </div>
                 </div>
             </main>
--- pages/projects.js
+++ pages/projects.js
@@ -18,58 +18,137 @@ export default function Projects(props) {
 
             <MContent>
                 <p>
-                    This page is intended to host and showcase the projects that
-                    I think are both noteworthy and easily displayable on the
-                    web. For a comprehensive list of my projects, please visit{" "}
-                    <a href="https://github.com/wilgao09" target="_blank">
+                    This page is intended to host and
+                    showcase the projects that I think are
+                    both noteworthy and easily displayable
+                    on the web. For a comprehensive list of
+                    my projects, please visit{" "}
+                    <a
+                        href="https://github.com/wilgao09"
+                        target="_blank"
+                    >
                         my github
                     </a>
-                    .
+                    . Some links are crossed out. This means
+                    they aren't deployed anymore, and won't
+                    be for the foreseeable future.
                 </p>
 
                 <h3> BetterChatRoom </h3>
                 <p>
-                    Here's a link to my chatroom project (I cannot guarantee the
-                    security of anything):{" "}
+                    Here's a link to my chatroom project (I
+                    cannot guarantee the security of
+                    anything):{" "}
                     <a
                         href="https://chatroom8982.herokuapp.com"
                         target="_blank"
-                        style={{ textDecoration: "line-through" }}
+                        style={{
+                            textDecoration: "line-through",
+                        }}
                     >
                         https://chatroom8982.herokuapp.com
                     </a>
                 </p>
 
                 <p>
-                    Heroku has ended its free service; this project won't be
-                    back up until I pay for a proper MongoDB+Digital Ocean
-                    instance (at that point, I might as well buy an OK domain
-                    name)
+                    Heroku has ended its free service; this
+                    project won't be back up until I pay for
+                    a proper MongoDB+Digital Ocean instance
+                    (at that point, I might as well buy an
+                    OK domain name)
                 </p>
 
                 <h3>Tracker</h3>
                 <p>
-                    Here's a link to my tracker project. The tracker project's
-                    objective is to aggregate data from users (by observing web
-                    traffic on websites) and to display it nicely. As of writing
-                    this, the Tracker project only tracks the traffic on my
-                    personal website. The project's backend is done in Java
-                    Spring Boot and MySQL; the frontend is done in Typescript,
-                    with the graphs done in VictoryJS. A link to the project can
-                    be found{" "}
+                    Here's a link to my tracker project. The
+                    tracker project's objective is to
+                    aggregate data from users (by observing
+                    web traffic on websites) and to display
+                    it nicely. As of writing this, the
+                    Tracker project only tracks the traffic
+                    on my personal website. The project's
+                    backend is done in Java Spring Boot and
+                    MySQL; the frontend is done in
+                    Typescript, with the graphs done in
+                    VictoryJS. A link to the project can be
+                    found{" "}
                     <a
                         href="https://tracker-dashboard.vercel.app"
                         target="_blank"
+                        style={{
+                            textDecoration: "line-through",
+                        }}
                     >
                         here
                     </a>
                     .
                 </p>
 
+                <h3>Playlister</h3>
+                <p>
+                    A music playing webapp. It leverages
+                    YouTube's Web API. The project is a
+                    M(ySQL)ERN stack, with the React portion
+                    done in TypeScript. Although it was
+                    largely a school project, it is
+                    important because it was the first
+                    project I did in TypeScript. Originally
+                    deployed with this website on a
+                    DigitalOcean droplet that I have since
+                    deleted.
+                </p>
+                <h3>DisOrganized</h3>
+                <p>
+                    Born out of the dissatisfaction with the
+                    state of online whiteboards, I built my
+                    own. It features exclusively local
+                    hosting and a custom plugin API so
+                    user's can develop their own tools. It
+                    was built using Golang, SvelteKit, and
+                    Electron, with data being stored in an
+                    SQLite file. A copy of the project can
+                    be found{" "}
+                    <a
+                        href="https://github.com/wilgao09/DisOrganized"
+                        target="_blank"
+                    >
+                        here
+                    </a>
+                </p>
+
+                <h3>autouml</h3>
+                <p>
+                    Inspired by{" "}
+                    <a
+                        href="https://github.com/terrastruct/d2"
+                        target="_blank"
+                    >
+                        terrastruct's d2 project
+                    </a>
+                    , this project seeks to programatically
+                    build out UML diagrams for TypeScript
+                    projects. It makes extensive use of the
+                    TypeScript compiler API, compiles
+                    information about classes, interfaces,
+                    and namespaces, and spits out d2 code.
+                    This d2 code compiles into a UML diagram
+                    of the input codebase. More information
+                    can be found on{" "}
+                    <a
+                        href="https://github.com/wilgao09/autouml"
+                        target="_blank"
+                    >
+                        its github page
+                    </a>
+                    .
+                </p>
+
                 <h3> More </h3>
                 <p>
-                    When I have projects that can be compiled to WebAssembly,
-                    they'll be showcased here or contain a link to them here.
+                    When I have projects that can be
+                    compiled to WebAssembly, they'll be
+                    showcased here or contain a link to them
+                    here.
                 </p>
             </MContent>
         </Layout>
